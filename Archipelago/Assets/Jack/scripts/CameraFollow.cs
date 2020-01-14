6497f099e2f0c1f60f29546b2f6b4199ce76ee5f
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-01-14 16:40:15 +0000

    cameraRotate1
    wont stop on hitting player

--- Archipelago/Assets/Jack/scripts/CameraFollow.cs
+++ Archipelago/Assets/Jack/scripts/CameraFollow.cs
@@ -10,6 +10,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float speed = 2.0f;
     [SerializeField] private float Zoffset = -7f;
     [SerializeField] private float Yoffset = 10f;
+    private float rotate = 0;
 
 
     private void Start()
@@ -22,6 +23,23 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         FollowObject();
+
+
+
+        if (Input.GetKey(KeyCode.H))
+        {
+            rotate -= Time.deltaTime * 5;
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            rotate += Time.deltaTime * 5;
+        }
+
+
+        if (rotate > 360) rotate -= 360;
+        if (rotate < 0) rotate += 360;
+        Debug.Log("Rotate: " + rotate);
     }
 
 
@@ -37,6 +55,9 @@ public class CameraFollow : MonoBehaviour
         position.x = Mathf.Lerp(transform.position.x, objectToFollow.transform.position.x, interpolation);
         position.y = Mathf.Lerp(transform.position.y, objectToFollow.transform.position.y + Yoffset, interpolation);
         this.transform.position = position;
+
+
+        //transform.root.Rotate(new Vector3(0,1,0), rotate);// = new Quaternion(0, rotate, 0, 1);
     }
 
 
