0df28fc901cebabc9a8720bbca64f25895bed836
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-03 10:49:12 +0000

    bloooomstep1

---
+++ Archipelago/Assets/Jack/terrain/New Terrain/BloomEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class BloomEffect : MonoBehaviour
+{
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        int width = source.width / 32;
+        int height = source.height / 32;
+        RenderTextureFormat format = source.format;
+
+
+        RenderTexture r = RenderTexture.GetTemporary(width, height, 0, format);
+
+        Graphics.Blit(source, r);
+        Graphics.Blit(r, destination);
+        RenderTexture.ReleaseTemporary(r);
+    }
+}
