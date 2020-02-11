a19fef484c4210358703373f18ade8bd87526eff
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-11 14:49:59 +0000

    skimming rocks raise and lower

---
+++ Archipelago/Assets/Jack/scripts/RaiseSkimRocks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseSkimRocks : MonoBehaviour
+{
+    Animator anim = null;
+
+    private void Start()
+    {
+        anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("RaiseRocks", true);
+        } 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("RaiseRocks", false);
+        }
+    }
+}
