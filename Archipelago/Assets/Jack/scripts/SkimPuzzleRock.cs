d1a92b7d3109c30007486f8458ea3ed106d4bb40
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-17 10:13:23 +0000

    skimstonetag

--- Archipelago/Assets/Jack/scripts/SkimPuzzleRock.cs
+++ Archipelago/Assets/Jack/scripts/SkimPuzzleRock.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class SkimPuzzleRock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Skimmer"))
+        {
+
+        }
     }
 }
