575ced0f15736fada2503644835f09bda1825ab6
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-18 10:31:54 +0000

    auto reference camera in compass

--- Archipelago/Assets/Jack/scripts/Compass.cs
+++ Archipelago/Assets/Jack/scripts/Compass.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Compass : MonoBehaviour
 {
-    [SerializeField] private GameObject Camera = null;
     [SerializeField] private GameObject NorthPole = null;
+    private GameObject cam = null;
+
+
+    private void Start()
+    {
+        cam = Camera.main.gameObject;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        var targetPosLocal = Camera.transform.InverseTransformPoint(NorthPole.transform.position);
+        //get the angle between the player and the north pole and rotate the compass so that it faces the north pole
+        var targetPosLocal = cam.transform.InverseTransformPoint(NorthPole.transform.position);
         var targetAngle = -Mathf.Atan2(targetPosLocal.x, targetPosLocal.z) * Mathf.Rad2Deg - 90;
         transform.eulerAngles = new Vector3(0, 0, targetAngle + 90);
     }
