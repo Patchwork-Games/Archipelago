5c43cc4d4db0cb480d663b90cae8848816dd7489
Aidan Murray <1702270@uad.ac.uk>
2020-02-19 17:08:13 +0000

    Sail now moves with the wind

--- Archipelago/Assets/Aidan/Scripts/WindManager.cs
+++ Archipelago/Assets/Aidan/Scripts/WindManager.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 public class WindManager : MonoBehaviour
 {
 	public Vector3 windDirection = Vector3.zero;
+	public Vector3 windDirectionRight = Vector3.zero;
 	public float windForce = 10f;
+
+	private void Start()
+	{
+		windDirectionRight = Vector3.Cross(windDirection, new Vector3(0,1,0));
+	}
 }
