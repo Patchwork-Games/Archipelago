94942d8135e5614dc04f347bed9fa10382be02a6
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-19 15:01:04 +0000

    island names shown when landing

---
+++ Archipelago/Assets/Jack/scripts/IslandName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IslandName : MonoBehaviour
+{
+    [SerializeField] private string islandName = "";
+    [SerializeField] private GameObject IslandNameUI = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //display the name of the island when entering shallow waters while in boat
+        if (other.CompareTag("Boat") && PlayerStateMachine.Instance.state == PlayerStateMachine.PlayerState.BOAT)
+        {
+            IslandNameUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = islandName;
+            IslandNameUI.SetActive(true);
+        }
+        
+    }
+}
