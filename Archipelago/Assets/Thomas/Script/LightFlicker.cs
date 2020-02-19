b18b694af13bfe4716adb946faaf246446ceb54c
THOMAS CROWE <1701698@uad.ac.uk>
2020-02-19 12:10:49 +0000

    Campfire Updates

---
+++ Archipelago/Assets/Thomas/Script/LightFlicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    Light campfireLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        campfireLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+  IEnumerator Flashing()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            campfireLight.enabled = !campfireLight.enabled;
+        }
+    }
+
+}
