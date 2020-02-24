707bb46462414e6e3021cf634fdcd9210582c92b
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-02-24 10:45:37 +0000

    MapMark1

--- Archipelago/Assets/Jack/scripts/MapControl.cs
+++ Archipelago/Assets/Jack/scripts/MapControl.cs
@@ -4,67 +4,27 @@ using UnityEngine;
 
 public class MapControl : MonoBehaviour
 {
-    Matrix4x4 mapWorldPos = new Matrix4x4
-        (
-        new Vector4(1, 0, 0, 0),
-        new Vector4(0, 1, 0, 0),
-        new Vector4(0, 0, 1, 0),
-        new Vector4(0, 0, 0, 1)
-        );
-
-    Matrix4x4 playerWorldPos = new Matrix4x4
-        (
-        new Vector4(1, 0, 0, 0),
-        new Vector4(0, 1, 0, 0),
-        new Vector4(0, 0, 1, 0),
-        new Vector4(0, 0, 0, 1)
-        );
-
-    Matrix4x4 playerMapPos = new Matrix4x4
-        (
-        new Vector4(1, 0, 0, 0),
-        new Vector4(0, 1, 0, 0),
-        new Vector4(0, 0, 1, 0),
-        new Vector4(0, 0, 0, 1)
-        );
-
-    [SerializeField] GameObject playerMapIcon;
-    [SerializeField] GameObject MapWorldMarker;
-
     // Start is called before the first frame update
     void Start()
     {
-        mapWorldPos = new Matrix4x4
-        (
-        new Vector4(1, 0, 0, -(MapWorldMarker.transform.position.x)),
-        new Vector4(0, 1, 0, -(MapWorldMarker.transform.position.y)),
-        new Vector4(0, 0, 1, 0),
-        new Vector4(0, 0, 0, 1)
-        );
+
     }
+
+
+
+    private void Update()
+    {
+        Vector3 newPos = StaticValueHolder.PlayerObject.transform.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //get players x and z position as x and y position
-        playerWorldPos = new Matrix4x4
-        (
-        new Vector4(1, 0, 0, StaticValueHolder.PlayerObject.transform.TransformPoint(transform.position).x),
-        new Vector4(0, 1, 0, StaticValueHolder.PlayerObject.transform.TransformPoint(transform.position).z),
-        new Vector4(0, 0, 1, 0),
-        new Vector4(0, 0, 0, 1)
-        );
 
-        playerMapPos = playerWorldPos * mapWorldPos;
-
-
-        Debug.Log("playerWorldPos: " + playerWorldPos);
-        Debug.Log("playerMapPos: " + playerMapPos);
-
-
-        Debug.Log("matrix: " + (playerWorldPos * playerMapPos));
-        Debug.Log("row: " + (playerWorldPos * playerMapPos).GetRow(3));
-        playerMapIcon.transform.position = playerMapIcon.transform.TransformPoint((playerWorldPos * playerMapPos).GetRow(3));
     }
 
 
