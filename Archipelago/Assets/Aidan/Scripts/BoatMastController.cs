5c5221a4bab4b6e3b4b86b78e1bc606173e7a1b9
Aidan Murray <1702270@uad.ac.uk>
2020-02-24 11:08:15 +0000

    Added FogWall prefab

---
+++ Archipelago/Assets/Aidan/Scripts/BoatMastController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMastController : MonoBehaviour
+{
+	[SerializeField] private Vector2 lockBetweenTwoRotations = Vector2.zero;
+	[SerializeField] private float rotationTime = 1f;
+	private float lerpTime = 0f;
+	private Quaternion newRotation = Quaternion.identity;
+	private Quaternion startingRotation = Quaternion.identity;
+
+	private void Start()
+	{
+		startingRotation = transform.localRotation;
+	}
+
+	private void Update()
+	{
+		float resultOfDotProd1 = Vector3.Dot(StaticValueHolder.WindManagerObject.windDirection, StaticValueHolder.BoatObject.transform.forward);
+		float resultOfDotProd2 = Vector3.Dot(StaticValueHolder.WindManagerObject.windDirectionRight, StaticValueHolder.BoatObject.transform.forward);
+
+		// Compare the wind direction vector to the boat's forward vector to determine the rotation of the mast
+		if (resultOfDotProd1 >= 0.5f)
+		{
+			if (resultOfDotProd2 >= 0)
+			{
+				SetRotation(Quaternion.Euler(0, -90, 0));
+			}
+			else
+			{
+				SetRotation(Quaternion.Euler(0, 90, 0));
+			}
+		}
+		else if (resultOfDotProd1 <= -0.5f)
+		{
+			SetRotation(Quaternion.Euler(0, 0, 0));
+		}
+		else if (resultOfDotProd1 >= -0.5f && resultOfDotProd1 <= 0.5f)
+		{
+			if (resultOfDotProd2 >= 0)
+			{
+				SetRotation(Quaternion.Euler(0, -45, 0));
+			}
+			else
+			{
+				SetRotation(Quaternion.Euler(0, 45, 0));
+			}
+		}
+
+
+		// Rotate towards the new rotation
+		lerpTime += Time.deltaTime / rotationTime;
+
+		if (lerpTime <= 1)
+		{
+			transform.localRotation = Quaternion.Lerp(startingRotation, newRotation, lerpTime);
+		}
+	}
+
+	private void SetRotation(Quaternion rotation)
+	{
+		startingRotation = transform.localRotation;
+		newRotation = rotation;
+		lerpTime = 0f;
+	}
+}
---
+++ Archipelago/Assets/Aidan/Scripts/FogWallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogWallController : MonoBehaviour
+{
+	//[SerializeField] float rangeToStartParticleSystem = 200f;
+ 	private ParticleSystem cloudParticles = null;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Boat"))
+		{
+			cloudParticles.gameObject.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Boat"))
+		{
+			cloudParticles.gameObject.SetActive(false);
+		}
+	}
+
+	private void Awake()
+	{
+		// Get the cloud particle system
+		cloudParticles = transform.Find("CloudParticles").GetComponent<ParticleSystem>();
+		if (cloudParticles == null)
+		{
+			Debug.Log("Cloud particles not found on object: " + this.gameObject);
+		}
+
+		cloudParticles.gameObject.SetActive(false);
+	}
+}
