5c5cacb6db37e20f4d01903f7d796853ad92ca58
Jack Chateau-Loney <1701009@uad.ac.uk>
2020-03-04 09:35:15 +0000

    fold all button

---
+++ Archipelago/Assets/Jack/Editor/FolderClose.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorUtils : Editor
+{
+    //This code is released under the MIT license: https://opensource.org/licenses/MIT
+    //Sources: https://forum.unity.com/threads/rel-collapse-all-gameobjects-recursively-in-hierarchy-view.298914/
+    //https://answers.unity.com/questions/656869/index.html#comment-858132
+    [MenuItem("Window/Fold all")]
+    static void UnfoldSelection()
+    {
+        EditorApplication.ExecuteMenuItem("Window/General/Hierarchy");
+        var hierarchyWindow = EditorWindow.focusedWindow;
+        var expandMethodInfo = hierarchyWindow.GetType().GetMethod("SetExpandedRecursive");
+        foreach (GameObject root in UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects())
+        {
+            expandMethodInfo.Invoke(hierarchyWindow, new object[] { root.GetInstanceID(), false });
+        }
+    }
+}
