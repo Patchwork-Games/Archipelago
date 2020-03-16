63990a05aa573fc2d0504de4a401979451f0b989
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2020-03-16 16:29:50 +0000

    Footprints on ground behind player

---
+++ Archipelago/Assets/Jack/scripts/FootPrintDelete.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootPrintDelete : MonoBehaviour
+{
+
+    float fadeTime = 1.0f;
+    Material footMat = null;
+
+    private void Awake()
+    {
+        footMat = GetComponent<Projector>().material = Instantiate<Material>(GetComponent<Projector>().material);
+    }
+
+
+    private void OnEnable()
+    {
+        fadeTime = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeTime > 0)
+        {
+
+            fadeTime -= Time.deltaTime / 2;
+            Debug.Log("FadeTime: " + fadeTime);
+            footMat.SetFloat("_Fade", fadeTime);
+        }
+        else gameObject.SetActive(false);
+
+    }
+}
---
+++ Archipelago/Assets/Jack/scripts/FootprintPlacer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootprintPlacer : MonoBehaviour
+{
+    private GameObject footL = null;
+    private GameObject footR = null;
+    private ObjectPooling spawner = null;
+    [SerializeField]private GameObject footCam = null;
+
+    private void Start()
+    {
+        footL = gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(1).GetChild(0).GetChild(0).GetChild(0).gameObject;
+        footR = gameObject.transform.GetChild(0).GetChild(3).GetChild(0).GetChild(2).GetChild(0).GetChild(0).GetChild(0).gameObject;
+
+        spawner = GameObject.FindGameObjectWithTag("FootSpawner").GetComponent<ObjectPooling>();
+    }
+
+
+
+
+    public void PlaceFootprintLeft()
+    {
+        GameObject newFootPrint = spawner.RetrieveInstance();
+        if (newFootPrint != null)
+        {
+            newFootPrint.transform.parent = spawner.transform;
+            newFootPrint.transform.position = footL.transform.position;
+            newFootPrint.transform.rotation = Quaternion.Euler(90, transform.rotation.eulerAngles.y, 0);
+        }
+    }
+
+
+    public void PlaceFootprintRight()
+    {
+        GameObject newFootPrint = spawner.RetrieveInstance();
+        if (newFootPrint != null)
+        {
+            newFootPrint.transform.parent = spawner.transform;
+            newFootPrint.transform.position = footR.transform.position;
+            newFootPrint.transform.rotation = Quaternion.Euler(90, transform.rotation.eulerAngles.y, 0);
+        }
+    }
+
+}
