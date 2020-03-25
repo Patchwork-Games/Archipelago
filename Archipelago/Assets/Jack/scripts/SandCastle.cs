5a939642a6f63d7cab80c39e86a4b2d876b22545
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2020-03-25 11:33:26 +0000

    sand castle

---
+++ Archipelago/Assets/Jack/scripts/SandCastle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCastle : MonoBehaviour
+{
+    ParticleSystem sand = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sand = transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().enabled = false;
+            sand.Play();
+        }
+    }
+}
