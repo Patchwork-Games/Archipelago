09ad1b7b097123226968af194eaa5571eedad8b1
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2020-04-01 15:36:10 +0100

    map fog

---
+++ Archipelago/Assets/Jack/scripts/MapSmoke.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSmoke : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+
+        if (collision.CompareTag("PlayerMapIcon"))
+        {
+            Debug.Log("Player hit");
+            Destroy(gameObject);
+        }
+    }
+}
