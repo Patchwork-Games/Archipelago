694ee3c7ad672f8e7141101648cfa2e68024b5e2
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2020-04-06 14:40:30 +0100

    fade out new discovered area smoke

--- Archipelago/Assets/Jack/scripts/MapSmoke.cs
+++ Archipelago/Assets/Jack/scripts/MapSmoke.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapSmoke : MonoBehaviour
 {
+    float fadeValue = 1.0f;
+    bool fading = false;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Hit");
-
-        if (collision.CompareTag("PlayerMapIcon"))
+        if (collision.CompareTag("PlayerMapIcon") && !fading)
         {
-            Debug.Log("Player hit");
-            Destroy(gameObject);
+            fading = true;
+            StartCoroutine(FadeSmoke()); 
         }
     }
+
+
+    //fade this smoke cloud over 1 second after it has been touched by the players icon on the map
+    IEnumerator FadeSmoke()
+    {
+        while (GetComponent<Image>().color.a > 0.0f)
+        {
+            fadeValue -= Time.deltaTime;
+            GetComponent<Image>().color = new Color(1, 1, 1, fadeValue);           
+            yield return null;
+        }
+
+        Destroy(gameObject);
+        
+    }
+
+
 }
