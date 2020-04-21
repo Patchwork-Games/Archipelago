4274fbaee6889658b558ad073f10b1fec8e1679b
Aidan Murray <1702270@uad.ac.uk>
2020-04-21 14:49:12 +0100

    Added the buoy course flags, changed the buoys to use and animation and randomised the animations for buoys and barrels

---
+++ Archipelago/Assets/Aidan/Scripts/AnimationOffset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOffset : MonoBehaviour
+{
+    private Animator animator = null;
+
+    private void OnEnable()
+    {
+        // Get the animator component
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.Log("Missing Animator component on object: " + gameObject);
+        }
+
+        // Set the random offset for the animation
+        animator.SetFloat("Offset", Random.Range(0.0f, 1.0f));
+    }
+}
